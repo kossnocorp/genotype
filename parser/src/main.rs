f89edd46dfdeccec336acc6cdee68415053986bc
Sasha Koss <koss@nocorp.me>
2024-09-07 15:16:21 +0800

    Basic grammar

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -9,30 +9,30 @@ use std::fs;
 pub mod parser;
 
 fn main() {
-    let unparsed_file = fs::read_to_string("numbers.csv").expect("cannot read file");
+    // let unparsed_file = fs::read_to_string("numbers.csv").expect("cannot read file");
 
-    let file = GenotypeParser::parse(Rule::file, &unparsed_file)
-        .expect("unsuccessful parse")
-        .next()
-        .unwrap();
+    // let file = GenotypeParser::parse(Rule::file, &unparsed_file)
+    //     .expect("unsuccessful parse")
+    //     .next()
+    //     .unwrap();
 
-    let mut field_sum: f64 = 0.0;
-    let mut record_count: u64 = 0;
+    // let mut field_sum: f64 = 0.0;
+    // let mut record_count: u64 = 0;
 
-    for record in file.into_inner() {
-        match record.as_rule() {
-            Rule::record => {
-                record_count += 1;
+    // for record in file.into_inner() {
+    //     match record.as_rule() {
+    //         Rule::record => {
+    //             record_count += 1;
 
-                for field in record.into_inner() {
-                    field_sum += field.as_str().parse::<f64>().unwrap();
-                }
-            }
-            Rule::EOI => (),
-            _ => unreachable!(),
-        }
-    }
+    //             for field in record.into_inner() {
+    //                 field_sum += field.as_str().parse::<f64>().unwrap();
+    //             }
+    //         }
+    //         Rule::EOI => (),
+    //         _ => unreachable!(),
+    //     }
+    // }
 
-    println!("Sum of fields: {}", field_sum);
-    println!("Number of records: {}", record_count);
+    // println!("Sum of fields: {}", field_sum);
+    // println!("Number of records: {}", record_count);
 }
--- parser/src/parser/mod.rs
+++ parser/src/parser/mod.rs
@@ -3,3 +3,35 @@ use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "parser/grammar.pest"]
 pub struct GenotypeParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pest::Parser;
+    use std::fs;
+
+    #[test]
+    fn test_alias() {
+        parse_file("../examples/basic/alias.type");
+    }
+
+    #[test]
+    fn test_struct() {
+        parse_file("../examples/basic/struct.type");
+    }
+
+    #[test]
+    fn test_comments() {
+        parse_file("../examples/basic/comments.type");
+    }
+
+    fn parse_file(file: &str) {
+        let file = fs::read_to_string(file).expect("cannot read file");
+        let parse = GenotypeParser::parse(Rule::file, &file);
+
+        if let Err(err) = parse {
+            println!("{:?}", err);
+            assert!(false);
+        }
+    }
+}
