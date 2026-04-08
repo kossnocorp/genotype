cad6ebfa4505894255668eee5b7702fc70ee1485
Sasha Koss <koss@nocorp.me>
2026-04-08 13:04:31 +0800

    chore: bootstrap website with Astro (closes #67)

---
+++ pkgs/app-website/astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Genotype",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/kossnocorp/genotype",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: [
+        "./src/styles/global.css",
+      ],
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
