118717652f7164093de9abd095e26f67d9fe65af
Sasha Koss <koss@nocorp.me>
2026-04-09 16:05:28 +0800

    chore: deploy basic landing page to https://genotype-lang.org/

--- pkgs/app-website/astro.config.mjs
+++ pkgs/app-website/astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -39,4 +41,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
